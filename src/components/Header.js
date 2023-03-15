0beec83caa7bf2628c0786dd6237d1ac7e2da2e3
felixesteban100 <felixesteban100@gmail.com>
2023-03-15 19:11:37 -0400

    changes in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='flex justify-start align-middle gap-3 bg-black p-5 border-b-1 cursor-pointer' onClick={() => {window.location.reload()}}>
-        <p className='text-white text-3xl'>Quizzical</p>
-        <img className='h-10' src="https://cdn-icons-png.flaticon.com/512/3406/3406898.png" alt="" />
+    <div className='bg-black p-5' >
+        <div className='flex cursor-pointer gap-3 w-fit' onClick={() => {window.location.reload()}}>
+          <p className='text-white text-3xl'>Quizzical</p>
+          <img className='h-10' src="https://cdn-icons-png.flaticon.com/512/3406/3406898.png" alt="" />
+        </div>
     </div>
   )
 }
